2c1b0dc3e34ff3095ea249df840a21e11fadaaf7
Berker Peksag <berker.peksag@gmail.com>
2015-06-25 16:03:30 +0300

    Fix typo.

--- mediagoblin/media_types/video/transcoders.py
+++ mediagoblin/media_types/video/transcoders.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import divisioin, print_function
+from __future__ import division, print_function
 
 import os
 import sys
