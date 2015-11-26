a6835f422f2b5af8df96cd958ffe2553ec9db15c
Boris Bobrov <breton@cynicmansion.ru>
2015-11-26 23:47:55 +0300

    Initial alembic migration to base other migrations on
    Use this migration as a parent for yours.

---
+++ mediagoblin/db/migrations/versions/52bf0ccbedc1_initial_revision.py
@@ -0,0 +1,22 @@
+"""initial revision
+
+Revision ID: 52bf0ccbedc1
+Revises: None
+Create Date: 2015-11-07 17:00:28.191042
+Description: This is an initial Alembic migration
+"""
+
+# revision identifiers, used by Alembic.
+revision = '52bf0ccbedc1'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
