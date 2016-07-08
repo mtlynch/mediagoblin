1ba4556b90730f94e8b173bf6282373d85a3e69e
saksham1115 <saksham115@gmail.com>
2016-07-08 19:47:35 +0000

    Removed ast and fixed comments

--- mediagoblin/tools/subtitles.py
+++ mediagoblin/tools/subtitles.py
@@ -1,26 +1,14 @@
-import ast,os
+import os
 
 def get_path(path):
-	"""
-    Converting the path of the form
-    x = u'[ "A","B","C" ," D"]'
-    to
-    x = ["A", "B", "C", "D"] 
-    """
-	return ast.literal_eval(path)
+	return eval(path)
 
 def open_subtitle(path):
 	temp = ['user_dev','media','public']
 	path = list(get_path(path))
 	file_path = os.path.abspath(__file__).split('/') # Path of current file as dictionary
-	"""
-	Creating the absolute path for the subtitle file
-	"""
-	subtitle_path = file_path[:-3] + temp + path
+	subtitle_path = file_path[:-3] + temp + path # Creating the absolute path for the subtitle file
 	subtitle_path = "/" + os.path.join(*subtitle_path)
-	"""
-	Opening the file using the absolute path
-	"""
-	subtitle = open(subtitle_path,"r")
+	subtitle = open(subtitle_path,"r") # Opening the file using the absolute path
 	text = subtitle.read()
 	return text
