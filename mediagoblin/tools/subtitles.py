4292c6aaaa1fb49740ecd0a26946f2460d805e46
saksham1115 <saksham115@gmail.com>
2016-07-13 13:36:14 +0000

    Modified get_path function in tools/subtitles.py

--- mediagoblin/tools/subtitles.py
+++ mediagoblin/tools/subtitles.py
@@ -1,23 +1,20 @@
 import os
 
 def get_path(path):
-	return eval(path)
-
-def open_subtitle(path):
 	temp = ['user_dev','media','public']
-	path = list(get_path(path))
+	path = list(eval(path))
 	file_path = os.path.abspath(__file__).split('/') # Path of current file as dictionary
 	subtitle_path = file_path[:-3] + temp + path # Creating the absolute path for the subtitle file
 	subtitle_path = "/" + os.path.join(*subtitle_path)
+	return subtitle_path
+
+def open_subtitle(path):
+	subtitle_path = get_path(path)
 	subtitle = open(subtitle_path,"r") # Opening the file using the absolute path
 	text = subtitle.read()
 	return text
 
 def save_subtitle(path,text):
-	temp = ['user_dev','media','public']
-	path = list(get_path(path))
-	file_path = os.path.abspath(__file__).split('/') # Path of current file as dictionary
-	subtitle_path = file_path[:-3] + temp + path # Creating the absolute path for the subtitle file
-	subtitle_path = "/" + os.path.join(*subtitle_path)
+	subtitle_path = get_path(path)
 	subtitle = open(subtitle_path,"w") # Opening the file using the absolute path
 	subtitle.write(text)
